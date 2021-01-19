d8d2ec0799d254147307247d069b487d2c200a85
ceponce <ceponce60@gmail.com>
2021-01-18 22:51:42 -0800

    for gcloud

--- src/main/java/heatwaves/backend/BackendApplication.java
+++ src/main/java/heatwaves/backend/BackendApplication.java
@@ -2,10 +2,9 @@ package heatwaves.backend;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
 
 @SpringBootApplication
-public class BackendApplication extends SpringBootServletInitializer {
+public class BackendApplication {
 
     public static void main(String[] args) {
         SpringApplication.run(BackendApplication.class, args);
